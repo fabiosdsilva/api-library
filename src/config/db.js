1a71c4a470d796ebff79223027fabaab8475b85d
AnnaSouza <164109781+Annas0321@users.noreply.github.com>
2025-01-16 00:43:55 -0300

    fix: corrigido erro ao se conectar com o banco de dados mysql

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,25 @@
 require('dotenv').config();
 
+const {
+  MYSQL_DIALECT,
+  MYSQL_USER,
+  MYSQL_HOST_NAME,
+  MYSQL_DATABASE,
+  MYSQL_PORT,
+  MYSQL_PASSWORD
+} = process.env;
+
+if (!MYSQL_USER || !MYSQL_DATABASE) {
+  throw new Error('As variáveis de ambiente MYSQL_USER e MYSQL_DATABASE são obrigatórias!');
+}
+
 module.exports = {
-  dialect: process.env.DIALECT,
-  username: process.env.USER_NAME,
-  password: process.env.PASSWORD,
-  host: process.env.HOST_NAME,
-  database: process.env.DATABASE,
-  port: process.env.PORT,
+  dialect: MYSQL_DIALECT,
+  username: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+  host: MYSQL_HOST_NAME,
+  database: MYSQL_DATABASE,
+  port: parseInt(MYSQL_PORT, 10),
   define: {
     timestamps: true,
     underscored: true,
