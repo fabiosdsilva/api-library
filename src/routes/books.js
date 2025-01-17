1a2d418c8dc4270b74f3c5d9686ce68db93b689a
AnnaSouza <164109781+Annas0321@users.noreply.github.com>
2025-01-17 02:29:50 -0300

    chore: concluido crud da tabela book

--- src/routes/books.js
+++ src/routes/books.js
@@ -31,4 +31,34 @@ routes.post('/books', (req, res) => {
       });
 });
 
+routes.put('/books/:id', (req, res) => {
+    const { title, author, genre, year } = req.body;
+    const { id } = req.params;
+    db.query('UPDATE books SET title = ?, author = ?, genre = ?, year = ? WHERE id = ?', [title, author, genre, year, id], (err, results) => {
+        if (err) {
+          console.error('Erro ao atualizar dados:', err);
+        } else {
+          console.log('Dados atualizados com sucesso!');
+        }
+        return res.json(results);
+
+        db.end(); 
+      });
+});
+
+
+routes.delete('/books/:id', (req, res) => { 
+    const { id } = req.params;
+    db.query('DELETE FROM books WHERE id = ?', [id], (err, results) => {
+        if (err) {
+          console.error('Erro ao deletar dados:', err);
+        } else {
+          console.log('Dados deletados com sucesso!');
+        }
+        return res.json(results);
+
+        db.end(); 
+      });
+});
+
 module.exports = routes;
